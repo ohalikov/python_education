5924e9b255e33994c575cbebcac7c50f588a0d47
ohalikov <olegway31@gmail.com>
2022-12-16 18:04:18 +0500

    change my operation generator and use fast api

---
+++ my_operations_generator/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from operations_generator import get_generation_operations
+
+my_own_api = FastAPI()
+
+
+@my_own_api.get("/")
+async def root():
+    return {"messages": "hello"}
+
+
+@my_own_api.get("/operations/{count_primes}")
+async def noroot(count_primes):
+    structure_ements = get_generation_operations(count_primes)
+    return structure_ements
--- my_operations_generator/operations_generator.py
+++ my_operations_generator/operations_generator.py
@@ -1,6 +1,8 @@
 import random
+import pprint
 
 RANGE_NUMBERS = {'start_range': 0, 'end_range': 20}
+RANGE_NUMBERS_MULTIPLY = {'start_range': 0, 'end_range': 10}
 COUNT_EXAMPLES = 20
 
 
@@ -31,7 +33,8 @@ def multiply(a, b):
 def division(a, b):
     if b == 0:
         b = get_new_value_not_equal_zero(RANGE_NUMBERS)
-    return a/b
+    remainder = a % b
+    return a//b, b, remainder
 
 
 OPERATORS_MAP = {
@@ -42,6 +45,75 @@ OPERATORS_MAP = {
 }
 
 
+def get_left_and_right_operands(dict_range_numbers, operator):
+    operand_1 = None
+    operand_2 = None
+    while True:
+        if operator == 'multiply':
+            operand_1 = get_random_value(
+                RANGE_NUMBERS_MULTIPLY)
+            operand_2 = get_random_value(
+                RANGE_NUMBERS_MULTIPLY)
+        else:
+            operand_1 = get_random_value(
+                dict_range_numbers)
+            operand_2 = get_random_value(
+                dict_range_numbers)
+
+        if operand_1 == 0:
+            operand_1 = None
+            continue
+
+        if operand_1 >= operand_2:
+            return operand_1, operand_2
+
+
+def get_inner_structure(left, right, operator, function):
+    right_operand = right
+    remainder = 0
+    if operator == 'division':
+        result_of_function, right_operand, remainder = function(left, right)
+    else:
+        result_of_function = function(left, right)
+
+    if remainder != 0:
+        inner_structure = {
+            "left_operand": left,
+            "right_operand": right_operand,
+            "operator": operator,
+            "result": result_of_function,
+            "division_remainder": remainder
+        }
+    else:
+        inner_structure = {
+            "left_operand": left,
+            "right_operand": right_operand,
+            "operator": operator,
+            "result": result_of_function
+        }
+    return inner_structure
+
+
+def get_random_operation():
+    return random.choice(list(OPERATORS_MAP.items()))
+
+
+def get_generation_operations(count):
+    structure_ements = []
+    for i in range(int(count)):
+
+        operator, calculating_function = get_random_operation()
+        left_operand, right_operand = get_left_and_right_operands(
+            RANGE_NUMBERS, operator)
+        inner_structure = get_inner_structure(
+            left_operand, right_operand, operator, calculating_function)
+
+        structure_ements.append(inner_structure)
+
+    return structure_ements
+
+
+# >>> MAIN FUNCTION <<<
 '''
     structure_elements = [
         {
@@ -51,39 +123,7 @@ OPERATORS_MAP = {
         },
     ]
 '''
-
-
-def get_left_and_right_operands(dict_range_numbers):
-    while True:
-        operand_1 = get_random_value(dict_range_numbers)
-        if operand_1 == 0:
-            continue
-
-        operand_2 = get_random_value(dict_range_numbers)
-        if operand_1 >= operand_2:
-            return operand_1, operand_2
-        # if operand_2 >= operand_1:
-        #     return operand_2, operand_1
-
-
-def get_generation_operations(count):
-    structure_ements = []
-    for i in range(count):
-        left_operand, right_operand = get_left_and_right_operands(
-            RANGE_NUMBERS)
-        key_operand, function_for_operand = random.choice(
-            list(OPERATORS_MAP.items()))
-
-        inner_structure = {
-            "left_operand": left_operand,
-            "right_operand": right_operand,
-            "operator": key_operand,
-            "result": function_for_operand(left_operand, right_operand)
-        }
-        structure_ements.append(inner_structure)
-    return structure_ements
-
-
 if __name__ == '__main__':
     structure_ements = get_generation_operations(COUNT_EXAMPLES)
-    print('structure_elements', structure_ements)
+    # pp = pprint.PrettyPrinter(width=60, compact=True)
+    # pp.pprint(structure_ements)
