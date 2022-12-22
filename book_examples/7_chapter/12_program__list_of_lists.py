b076bee409a9ae9bba59a8adf3ee98091341ccd9
ohalikov <olegway31@gmail.com>
2022-12-22 17:15:19 +0500

    7 chapter...

---
+++ book_examples/7_chapter/12_program__list_of_lists.py
@@ -0,0 +1,15 @@
+small_birds = ['hummingbird', 'finch']
+extinct_birds = ['dodo', 'passenger pigeon', 'Norwegian Blue']
+carol_birds = [3, 'French hens', 2, 'turtledoves']
+all_birds = [small_birds, extinct_birds, 'macaw', carol_birds]
+
+print(all_birds)
+
+
+# Включения кортежей
+# Изменяемые типы (списки словари и множества) имеют включения
+# Неизменяемые типы строки и кортежи нужно создавать другими методами
+
+#
+# Генератор - это один из способов
+# предоставления данных итератору.
