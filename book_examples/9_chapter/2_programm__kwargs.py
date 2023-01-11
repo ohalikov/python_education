ff94a10dcf6bccfaffa128a3a0040a75054ee156
ohalikov <olegway31@gmail.com>
2023-01-11 18:24:14 +0500

    learn **kwargs

--- book_examples/9_chapter/2_programm__kwargs.py
+++ book_examples/9_chapter/2_programm__kwargs.py
@@ -1,5 +1,16 @@
-def print_kwargs(**kwargs):
-    print('keyword arguments:', kwargs)
+# def print_kwargs(**kwargs):
+#     print(list(kwargs))
+#     print('keyword arguments:', kwargs)
     
 
-print_kwargs(wine='merlot', entree='mutton', dessert='macaroon')
+# print_kwargs(wine='merlot', entree='mutton', dessert='macaroon')
+
+
+# =================================
+def print_data(data, *, start=0, end=100):
+    for value in (data[start:end]):
+        print(value)
+        
+
+data = ['a', 'b', 'c', 'd', 'e', 'f']
+print_data(data)
